37681c45b1fb0aa6fc94cb348218993051a9c7e4
Rizwan Munawar <Rizwan.munawar143@gmail.com>
2026-04-07 19:41:18 +0100

    Whie.loop 1st price day

--- tradingbot.py
+++ tradingbot.py
@@ -3,18 +3,22 @@ import streamlit as st
 st.title("My First App")
 st.write("Hello Rizwan!")
 
-coin ="BTC"
-price = 59000
+import time
 
-if price == 60000:
-    st.write("Perfect Entry:",price)
-elif price < 60000:    
-    st.write('BUY',coin,'at price',price)
-elif price > 65000:
-    st.write('Sell',coin,'at price', price)   
-else:
-    st.write('Wait - current price', price)
+coin = "BTC"
+price= 65000
 
+while True:
+    print("Price:",price)
 
+    if price > 65000:
+        print("SELL")
+    elif price <60000:
+        print("BUY")
+    else:
+        print("WAIT")
+
+    price = price - 100 #simulate price change
+    time.sleep(2)
     
 
