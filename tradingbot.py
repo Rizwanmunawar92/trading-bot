95b0794943c5bba236615b4e4f2ad6edc398f689
Rizwan Munawar <Rizwan.munawar143@gmail.com>
2026-04-09 19:35:12 +0100

    Defined and used function with while loop

--- tradingbot.py
+++ tradingbot.py
@@ -5,27 +5,58 @@ st.write("Hello Rizwan!")
 
 import time
 
-coin = "BTC"
-price = 65000
-
-while True:
-    print("price :",price,time.ctime()) # time.ctime used to print readable time on the terminal
-    
-    if price < 60000:
-        print("Buy", price)
+def trading_decision(price): #function refined
+    if price == 60000: #rules for the function defined from line 9 - 16.
+        print("Perfect Entry")
+    elif price < 60000:
+        print("BUY")
     elif price > 65000:
-        print("SELL:",price)
+        print("SELL")
     else:
         print("WAIT")
 
-    price += 200 #will simulate price
 
-    if price >= 66000:
-        print("Stopping loop - Target reached")
+price = 58000  # current price, in future I will try and learn to get this price from the real plateform 
+
+# while loop current defined
+while True:  
+    print("Price:",price, "Time:", time.ctime()) # code added to print price with current time. 
+    trading_decision(price)   # above define function called here.
+    price +=200     #simulating price  but in future I will make it to track real time data/price/
+
+    if price >=66000:     #loop break condition set 
+        print("Stopping loop - target reached")
         break
 
+    time.sleep(2)         # function called to run code every 2 seconds
 
-    time.sleep(2)
 
+
+        # Code add below is to practice use coin name and price in the defined funtion. 
     
+    '''import time
+    def trading_decision(coin, price):
+    print(f"{coin} price: {price}")
+    if price == 60000:
+        print("Perfect Entry")
+    elif price < 60000:
+        print("BUY")
+    elif price > 65000:
+        print("SELL")
+    else:
+        print("WAIT")
 
+coin = "BTC"
+price = 58000
+
+while True:
+    trading_decision(coin, price)
+
+    price += 200  # simulate price
+
+    if price >= 66000:
+        print("Stopping loop - target reached")
+        break
+
+    print(f"Time: {time.ctime()}")
+    time.sleep(2) '''
